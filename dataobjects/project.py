b304d16ba7d322ab72937c5b53639c7bb78ea07f
guiderae <guiderae@yahoo.com>
2023-02-22 13:18:07 -0700

    New class data object for Project

---
+++ dataobjects/project.py
@@ -0,0 +1,6 @@
+class Project:
+    def __init__(self, projectid, teamid, modelid, projectname):
+        self.projectid = projectid
+        self.teamid = teamid
+        self.modelid = modelid
+        self.projectname = projectname
