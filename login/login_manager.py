19d9027c57cf5ffb4262dec0fc2b0414e0ecacea
guiderae <guiderae@yahoo.com>
2023-02-22 13:15:46 -0700

    Added documentation.

--- login/login_manager.py
+++ login/login_manager.py
@@ -1,13 +1,21 @@
-from dbservices.user_service import User_service
+from dbservices.user_service import UserService
+
+
 class LoginManager():
 
     @staticmethod
     def login_validation(username, password):
-        user = User_service.get_user_by_username(username)
-        if(user is not None):
+        """
+        Validate username/password
+        :param username: Login username
+        :param password: Login password
+        :return: String message indicating login result.
+        """
+        user = UserService.get_user_by_username(username)
+        if user is not None:
             if user.password == password:
-                return 'ok'
+                return 'ok', user
             else:
-                return 'Incorrect Password'
+                return 'Incorrect Password', None
         else:
-            return 'User ' + username + ' not found'
+            return 'User ' + username + ' not found', None
