8fbb9d721ee87c0666ae1fdac2b48e7400ae0fca
guiderae <guiderae@yahoo.com>
2023-02-22 14:31:10 -0700

    New Team data object

---
+++ dataobjects/team.py
@@ -0,0 +1,7 @@
+
+class Team:
+    def __init__(self, teamid, teamname, department, teamlead_userid):
+        self.teamid = teamid
+        self.teamname = teamname
+        self.department = department
+        self.teamlead_userid = teamlead_userid
