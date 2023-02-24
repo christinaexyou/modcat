94aa8240c3ba9bc9de5452692c06913744e50ddb
guiderae <guiderae@yahoo.com>
2023-02-24 08:38:13 -0700

    New data object for projectsmodels table

---
+++ dataobjects/projects_models.py
@@ -0,0 +1,7 @@
+
+class ProjectsModels:
+    def __init__(self, projectsmodelsid, projectid, modelid, timestamp=None):
+        self.projectsmodelsid = projectsmodelsid
+        self.projectid = projectid
+        self.modelid = modelid
+        self.timestamp = timestamp
