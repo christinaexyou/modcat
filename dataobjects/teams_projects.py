5acd923130186e2a572cb84330aeb7b0e83e0831
guiderae <guiderae@yahoo.com>
2023-02-24 08:37:48 -0700

    Data object for teamsprojects table

---
+++ dataobjects/teams_projects.py
@@ -0,0 +1,8 @@
+
+class TeamsProjects:
+    def __init__(self, teamsprojectsid, teamid, projectid, timestamp=None):
+        self.teamsprojectsid = teamsprojectsid
+        self.teamid = teamid
+        self.projectid = projectid
+        self.timestamp = timestamp
+
