180bc54f0041968a29dcc21ae57ea6e71999ae4d
guiderae <guiderae@yahoo.com>
2023-02-24 08:38:47 -0700

    New data object for modelsusers table

---
+++ dataobjects/models_users.py
@@ -0,0 +1,7 @@
+
+class ModelsUser:
+    def __init__(self, modelsusersid, modelid, userid, timestamp=None):
+        self.modelsusersid = modelsusersid
+        self.modelid = modelid
+        self.userid = userid
+        self.timestamp = timestamp
