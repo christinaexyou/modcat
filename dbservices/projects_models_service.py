76f72e3697f8d2607455c3e33e5a146239941076
guiderae <guiderae@yahoo.com>
2023-02-25 16:34:38 -0700

    Added get_models_by_team()

--- dbservices/projects_models_service.py
+++ dbservices/projects_models_service.py
@@ -1,12 +1,17 @@
 import psycopg2
 from services.connection_pool_singleton import ConnectionPoolSingleton
 from dataobjects.dbobject_utils import DBObjectUtils
+import pandas as pd
 
 
 class ProjectsModelsService:
 
     GET_ALL_PROJECTS_MODELS = """select * from projectsmodels order by timestamp"""
     GET_PROJECTS_BY_MODELS = """select projectid from projectsmodels where modelid=%s"""
+    GET_MODELS_BY_TEAMS = """select projects.projectid, models.modelid, modelname  from projects, models, 
+                                projectsmodels where models.modelid = projectsmodels.modelid and
+                                projects.projectid = projectsmodels.projectid and projects.projectid 
+                                in (select projectid from teamsprojects where teamid in {teamids})"""
 
     @staticmethod
     def get_all_projects_models():
@@ -54,6 +59,35 @@ class ProjectsModelsService:
                 projectid = row[0]
                 model_id_list.append(projectid)
             return model_id_list
+        except (Exception, psycopg2.DatabaseError) as err:
+            print(err)
+
+        finally:
+            if cursor is not None:
+                cursor.close()
+            pool.putconn(conn)
+
+    @staticmethod
+    def get_models_by_teams(teamids):
+        pool = ConnectionPoolSingleton.getConnectionPool()
+        conn = pool.getconn()
+
+        dict_array = []
+        try:
+            cursor = conn.cursor()
+            # The execute() method's second parameter is a tuple that contains the param value of the user id.
+            final_sql = ProjectsModelsService.GET_MODELS_BY_TEAMS.format(teamids=tuple(teamids))
+            print(cursor.mogrify(final_sql, teamids).decode('utf8'))
+            cursor.execute(final_sql, teamids)
+            rows = cursor.fetchall()
+            for row in rows:
+                projectid = row[0]
+                modelid = row[1]
+                modelname = row[2]
+                row_dict = {'projectid': projectid, 'modelid': modelid, 'modelname': modelname}
+                dict_array.append(row_dict)
+
+            return pd.DataFrame.from_records(dict_array)
         except (Exception, psycopg2.DatabaseError) as err:
             print(err)
 
