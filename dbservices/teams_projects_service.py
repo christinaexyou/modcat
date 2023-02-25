ccc06f3790237728e99837643ce04eac7be139dd
guiderae <guiderae@yahoo.com>
2023-02-25 16:37:03 -0700

    Added get_project_ids_by_team()

--- dbservices/teams_projects_service.py
+++ dbservices/teams_projects_service.py
@@ -1,14 +1,23 @@
 
 
 import psycopg2
+from psycopg2 import sql
 from services.connection_pool_singleton import ConnectionPoolSingleton
 from dataobjects.dbobject_utils import DBObjectUtils
+import pandas as pd
 
 
 class TeamsProjectsService:
 
     GET_ALL_TEAMS_PROJECTS = """select * from teamsprojects order by timestamp"""
-    GET_PROJECTS_BY_TEAM = """select projectid from teamsprojects where teamid=%s"""
+    GET_PROJECT_IDS_BY_TEAM = """select projectid from teamsprojects where teamid=%s"""
+    GET_PROJECTS_BY_TEAMS = """select teamsprojects.teamid,projects.projectid, projectname   
+                                    from projects,   teamsprojects  
+                                    where 
+                                        projects.projectid = teamsprojects.projectid and projects.projectid 
+                                        in (select projectid from teamsprojects where teamid in {teamids}) 
+                                    order by teamsprojects.teamid;"""
+
 
     @staticmethod
     def get_all_teams_projects():
@@ -37,20 +46,55 @@ class TeamsProjectsService:
             pool.putconn(conn)
 
     @staticmethod
-    def get_projectids_by_team_id(teamid):
+    def get_project_df(teamids):
         """
         Get a list of project ids with the given teamid from the TEAMSPROJECTS table
-        :param teamid: Id of team
-        :return: List that contains all project ids for the given teamid
-                 Return Empty list if no modelids exists with given teamid
+        :param teamids: tuple of team ids
+        :return: DataFrame
         """
         pool = ConnectionPoolSingleton.getConnectionPool()
         conn = pool.getconn()
+
+        dict_array = []
+        try:
+            cursor = conn.cursor()
+            # The execute() method's second parameter is a tuple that contains the param value of the user id.
+            final_sql = TeamsProjectsService.GET_PROJECTS_BY_TEAMS.format(teamids=tuple(teamids))
+
+            print(cursor.mogrify(final_sql).decode('utf8'))
+            cursor.execute(final_sql)
+            rows = cursor.fetchall()
+            for row in rows:
+                teamid = row[0]
+                projectid = row[1]
+                projectname = row[2]
+                row_dict = {'teamid': teamid, 'projectid': projectid, 'projectname': projectname}
+                dict_array.append(row_dict)
+
+            return pd.DataFrame.from_records(dict_array)
+        except (Exception, psycopg2.DatabaseError) as err:
+            print(err)
+
+        finally:
+            if cursor is not None:
+                cursor.close()
+            pool.putconn(conn)
+
+    @staticmethod
+    def get_project_ids_by_team(teamid):
+        """
+
+        :param teamids: tuple of teamids
+        :return:
+        """
+        pool = ConnectionPoolSingleton.getConnectionPool()
+        conn = pool.getconn()
+
         project_id_list = []
         try:
             cursor = conn.cursor()
             # The execute() method's second parameter is a tuple that contains the param value of the user id.
-            cursor.execute(TeamsProjectsService.GET_PROJECTS_BY_TEAM, (teamid,))
+            cursor.execute(TeamsProjectsService.GET_PROJECT_IDS_BY_TEAM, teamid)
             rows = cursor.fetchall()
             for row in rows:
                 projectid = row[0]
